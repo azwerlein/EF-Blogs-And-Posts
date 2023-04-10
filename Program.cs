3359e33b28b8ca95034f09922513142d3f910084
Archie <azwerlein@my.wctc.edu>
2023-04-10 14:12:23 -0500

    Allow the program to create blogs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,51 @@
 ﻿using NLog;
 
-string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+public class BlogPosts
+{
+    public static Logger Logger = LogManager.LoadConfiguration(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome!");
 
-// create instance of Logger
-var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
-logger.Info("Hello world!");
+        Console.WriteLine("Please enter your selection.");
+        Console.WriteLine("1) Display all blogs");
+        Console.WriteLine("2) Add blog");
+        Console.WriteLine("3) Create Post");
+        Console.WriteLine("4) Display Posts");
+
+        string response = Console.ReadLine();
+
+        switch (response)
+        {
+            case "1":
+                Console.WriteLine("[Display blogs]");
+                break;
+            case "2":
+                CreateBlog();
+                break;
+            case "3":
+                Console.WriteLine("[Create post]");
+                break;
+            case "4":
+                Console.WriteLine("[Display posts]");
+                break;
+            default:
+                Console.WriteLine("Default response");
+                break;
+        }
+
+        Console.WriteLine("Have a good one!");
+    }
+
+    private static void CreateBlog()
+    {
+        Console.Write("Enter a name for the new Blog: ");
+        string name = Console.ReadLine();
+
+        Blog blog = new Blog { Name = name };
+
+        BloggingContext db = new BloggingContext();
+        db.AddBlog(blog);
+        Logger.Info("Blog added - {name}", name);
+    }
+}
