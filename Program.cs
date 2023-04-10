e1bb0cbec5cded8848b07ba5e812fe3307fd51f8
Archie <azwerlein@my.wctc.edu>
2023-04-10 14:23:32 -0500

    Display existing blogs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using NLog;
+using System.Linq;
 
 public class BlogPosts
 {
@@ -7,44 +8,69 @@ public class BlogPosts
     {
         Console.WriteLine("Welcome!");
 
-        Console.WriteLine("Please enter your selection.");
-        Console.WriteLine("1) Display all blogs");
-        Console.WriteLine("2) Add blog");
-        Console.WriteLine("3) Create Post");
-        Console.WriteLine("4) Display Posts");
+        BloggingContext db = new BloggingContext();
 
-        string response = Console.ReadLine();
-
-        switch (response)
+        bool loop = true;
+        while (loop)
         {
-            case "1":
-                Console.WriteLine("[Display blogs]");
-                break;
-            case "2":
-                CreateBlog();
-                break;
-            case "3":
-                Console.WriteLine("[Create post]");
-                break;
-            case "4":
-                Console.WriteLine("[Display posts]");
-                break;
-            default:
-                Console.WriteLine("Default response");
-                break;
+            Console.WriteLine("Please enter your selection.");
+            Console.WriteLine("1) Display all blogs");
+            Console.WriteLine("2) Add blog");
+            Console.WriteLine("3) Create post");
+            Console.WriteLine("4) Display posts");
+            Console.WriteLine("5) Exit the program");
+
+            string response = Console.ReadLine();
+            try
+            {
+                switch (response)
+                {
+                    case "1":
+                        DisplayBlogs(db);
+                        break;
+                    case "2":
+                        CreateBlog(db);
+                        break;
+                    case "3":
+                        Console.WriteLine("[Create post]");
+                        break;
+                    case "4":
+                        Console.WriteLine("[Display posts]");
+                        break;
+                    case "5":
+                        loop = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid response!");
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e.Message);
+            }
         }
 
         Console.WriteLine("Have a good one!");
     }
 
-    private static void CreateBlog()
+    private static void DisplayBlogs(BloggingContext db)
+    {
+        var query = db.Blogs.OrderBy(b => b.Name);
+
+        Console.WriteLine("All blogs in the database:");
+        foreach (var item in query)
+        {
+            Console.WriteLine(item.Name);
+        }
+    }
+
+    private static void CreateBlog(BloggingContext db)
     {
         Console.Write("Enter a name for the new Blog: ");
         string name = Console.ReadLine();
 
         Blog blog = new Blog { Name = name };
-
-        BloggingContext db = new BloggingContext();
         db.AddBlog(blog);
         Logger.Info("Blog added - {name}", name);
     }
